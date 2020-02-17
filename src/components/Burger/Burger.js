cf2988eba8220ee1067626e5f23ed00cf5cc4d1e
Abhilash <abhilash.@Abhilashs-MacBook-Pro.local>
2020-02-17 20:06:29 +0530

    Changes for dynamic ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,24 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients) //returns an array of keys
+                .map (ingKey => {
+                    return [...Array(props.ingredients[ingKey])] //creating a new array of size equal to the value
+                    .map((_,index) => {
+                        return <BurgerIngredient key={ingKey+index} type={ingKey}/>
+                    });
+                }).reduce((preVal, el) => {
+                    return preVal.concat(el)
+                }, []);
+    //the reduce method is similar to flatMap method in lambda            
+    //console.log(transformedIngredients);
+
+    if (transformedIngredients.length === 0 )
+        transformedIngredients = <p>Please start adding ingredients</p>
     return (
         <div className = {classes.Burger}>
             <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='salad'/>
-            <BurgerIngredient type='cheese'/>
+                {transformedIngredients}
             <BurgerIngredient type='bread-bottom'/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,23 @@
 //we impot React to use JSX with components
 import React, {Component} from 'react';
-import Aux from '../../hoc/Auxiliary';
+import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    //object with ingredient and their counts
+    state = {
+        ingredients : {
+            'bacon' : 0,
+            'salad' : 0,
+            'cheese' : 0,
+            'meat' : 0
+        }
+    }
 
     render () {
         return (
             <Aux>
-                <Burger/>
+                <Burger ingredients = {this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
