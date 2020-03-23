91dd709c61b4836228a75ce3665f1b165932f897
Abhilash <abhilash.@Abhilashs-MacBook-Pro.local>
2020-03-23 20:37:18 +0530

    Lazy loading the components

--- src/App.js
+++ src/App.js
@@ -2,32 +2,55 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import ErrorHandler from './hoc/ErrorHandler/ErrorHandler';
-import Checkout from './containers/Checkout/Checkout';
-import {Route, Switch, withRouter} from 'react-router-dom';
-import Orders from '../src/containers/Orders/Orders';
+import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import {connect} from 'react-redux';
 import * as actions from './store/actions/index';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+//lazy loading checkout component just as an example to load these components only when needed
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import('../src/containers/Orders/Orders');
+});
 
 class App extends Component {
 
-  componentDidMount() {
+  componentDidMount () {
     this.props.onTryAutoSignUp();
   }
 
-  render() {
+  render () {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if ( this.props.isAuthenticated ) {
+      routes = (
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/orders" component={asyncOrders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
     return (
       <div>
         <Layout>
           <ErrorHandler>
-            <Switch>
-              <Route path="/" exact component={BurgerBuilder}/>
-              <Route path="/orders" component={Orders}/>
-              <Route path="/checkout" component={Checkout}/>
-              <Route path="/auth" component={Auth}/>
-              <Route path="/logout" component={Logout}/>
-            </Switch>
+            {routes}
           </ErrorHandler>
         </Layout>
       </div>
